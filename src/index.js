caa21987e4a7de613c3fc78e109fd806f88a859c
eriksape <erik.sape@gmail.com>
2020-02-05 23:16:03 -0600

    adding fibonacci function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const fibonacci = (n) => {
-
+  const result = [0,1]
+  for (let i = 2; i <= n; i++) {
+    result.push(result[i-1] + result[i-2]);
   }
+  result.shift(0);
+  return result;
+}
 
-
-  module.exports = fibonacci;
+module.exports = fibonacci;
