30907cc3aa41ea370298d6c0a94716b87a1650c0
Jefferson Aguilar <jefferson@alunices.com>
2020-02-05 21:28:29 -0500

    Implementación de la serie de fibonacci

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const fibonacci = (n) => {
-
+  let fibonacciSerie=[0, 1];
+  for (let i = 1; i < n; i++) {
+    fibonacciSerie.push(fibonacciSerie[i] + fibonacciSerie[i-1]);
   }
+  fibonacciSerie.shift();
+  return fibonacciSerie;
+}
 
-
-  module.exports = fibonacci;
+module.exports = fibonacci;
