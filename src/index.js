2da3daa5dea15a05a7e99a82c008df8fc80f9f2c
Jorge Humberto Nemoga Pinzon <jhnemogap@gmail.com>
2020-02-06 06:11:32 -0500

    fix in one line .forEach (line 7 index.js)

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ const fibonacci = (n) => {
   if ( Number.isSafeInteger(n) ) {
     ary = Array(Math.abs(n)).fill(1);
     ary.unshift(0);
-    ary.forEach((v, i, a) => 
-      i >= 2 ? a[i] = a[i - 2] + a[i - 1] : ''
-    );
+    ary.forEach((v, i, a) => i >= 2 ? a[i] = a[i - 2] + a[i - 1] : '');
   }
     
   return (
