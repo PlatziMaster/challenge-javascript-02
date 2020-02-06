b8ddaad0ea161f020e2aa7a843d9f7356d514f09
Iván D. Sánchez <cosmosoft.soluciones@gmail.com>
2020-02-05 22:25:10 -0500

    Solución reto 2 del programa PlatziMaster- Serie Fibonacci

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-const fibonacci = (n) => {
-
+const fibonacci = n => {
+  const res = [];
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    if (i === 1) {
+      res.push(i);
+    } else if (i === 2) {
+      res.push((sum += 1));
+    } else {
+      res.push(res[res.length - 1] + res[res.length - 2]);
+    }
+    console.log(res);
   }
+  return res;
+};
 
-
-  module.exports = fibonacci;
+module.exports = fibonacci;
