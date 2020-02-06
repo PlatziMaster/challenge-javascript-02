3f581923bb81e9e6c7cf51417e906719a727302b
Jorge Humberto Nemoga Pinzon <jhnemogap@gmail.com>
2020-02-06 04:10:58 -0500

    Fibonacci resolved, start from 1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const fibonacci = (n) => {
+  const ary = Array(n).fill(1,0,n);
 
-  }
+  ary.forEach((val, index, ar) => 
+    index >= 2 ? ar[index] = ar[index - 2] + ar[index - 1] : ''
+  );
+  
+  return ary;
+};
 
-
-  module.exports = fibonacci;
+module.exports = fibonacci;
