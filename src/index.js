2981b503a152f842581e1990256a9f66876886a1
Alexis Araujo <ala1289@gmail.com>
2020-02-19 09:51:02 -0600

    Se agrega la funcionalidad para la serie de fibonacci

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-const fibonacci = (n) => {
-
+const fibonacci = n => {
+  const result = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    result.push(result[i - 1] + result[i - 2]);
   }
+  result.shift(0);
+  console.log(result);
+  return result;
+};
 
-
-  module.exports = fibonacci;
+module.exports = fibonacci;
