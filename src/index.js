8efe250728d4b9bd15cae23019b7fed52fc9da56
luis_parra <luis.parra.pulido@gmail.com>
2020-02-20 21:17:23 -0500

    reto terminado

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-const fibonacci = (n) => {
-
+const fibonacci = (number) => {
+  let n = number
+  const result = []
+  let a = 0;
+  let b = 1;
+  let c = 1;
+  for ( let i = 1; i <= n; i++) {
+    result.push(c)
+    c = a + b;
+    a = b;
+    b = c;  
   }
+  return result
+}
 
 
   module.exports = fibonacci;
