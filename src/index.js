f3e63d0f01d5ae1680ea85784abb69af45ae3313
Kristian Lopez <kristian.lom@gmail.com>
2020-03-04 10:40:09 -0600

    Reto solucionado

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 const fibonacci = (n) => {
 
+  let a = 0;
+  let b = 1;
+  let c = 0;
+  let array = new Array(n);
+
+  for (let i = 0; i < n; ) {
+    c = a + b;
+    a = b;
+    b = c;
+    array[i] = c;
+    array[i++] = a;
   }
+  
+  return array;
+}
 
 
-  module.exports = fibonacci;
+module.exports = fibonacci;
