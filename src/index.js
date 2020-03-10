7b1f3a0e45d8c9fe4a9d4c1ca63da42cc9c05454
= <jonne@LAPTOP-V6AUKDD4.localdomain>
2020-03-10 03:31:53 -0600

    Ejercicio de serie fibonacci con recursión completado, test de la prueba aprobado

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 const fibonacci = (n) => {
+    let fibArray = [];
+    for (let i = 1; i < n + 1; i++) {
+      fibArray.push(recursive(i));
+    }
+  
+    return fibArray;
+  };
+  
+  let recursive = function(i) {
+    if (i < 2) {
+      return i;
+    }
+    else {
+      return recursive(i - 1) + recursive(i - 2);
+    }
+  };
+  
+  console.log(fibonacci(9));
 
-  }
-
-
-  module.exports = fibonacci;
+module.exports = fibonacci;
