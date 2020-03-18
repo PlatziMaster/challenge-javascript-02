c346e314c04170cc3b7b004b84369db1febdcfee
ThAlanD <alandavidrl11@gmail.com>
2020-03-18 10:32:22 -0600

    JavaScript fibonacci challenge done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 const fibonacci = (n) => {
+  let result = [1];
 
+  for (let i = 1; i < n; i++) {
+    if (i <= 1) result.push(1);
+    else result.push(result[i - 1] + result[i - 2]);
   }
 
+  return result;
+}
 
-  module.exports = fibonacci;
+module.exports = fibonacci;
