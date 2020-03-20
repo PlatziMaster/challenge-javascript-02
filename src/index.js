d31edc0ae03f285fbb2fe6b17101ee6de88f08a8
Juan Jose Mayorga <tmaniacus@gmail.com>
2020-03-19 20:23:05 -0600

    challenge done, corregido

--- src/index.js
+++ src/index.js
@@ -1,30 +1,20 @@
 const fibonacci = (n) => {
 
-    let array = []
+    let array = [];
     let num1 = 0;
     let num2 = 1;
 
-    let add = num1 + num2
-
-    array.push(num1, num2)
+    let add = num1 + num2;
+    array.push(num1, num2);
 
     for (let i = 2; i < n; i++) {
+        add = num1 + num2;
+        array.push(add);
 
-        add = num1 + num2
-        array.push(add)
-
-        num1 = num2
-        num2 = add
-
+        num1 = num2;
+        num2 = add;
     }
-
     return array
-
 }
-
-//fibonacci(15);
-console.log(fibonacci(20));
-
-
 
 module.exports = fibonacci;
