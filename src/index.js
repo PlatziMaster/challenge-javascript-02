98563676fa973a1173bfa1fa425b1bf2f6a5efb3
Juan Jose Mayorga <tmaniacus@gmail.com>
2020-03-19 20:07:06 -0600

    reto cumplido, fibonacci

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
 const fibonacci = (n) => {
 
-  }
+    let array = []
+    let num1 = 0;
+    let num2 = 1;
+
+    let add = num1 + num2
+
+    array.push(num1, num2)
+
+    for (let i = 2; i < n; i++) {
+
+        add = num1 + num2
+        array.push(add)
+
+        num1 = num2
+        num2 = add
+
+    }
+
+    return array
+
+}
+
+//fibonacci(15);
+console.log(fibonacci(20));
 
 
-  module.exports = fibonacci;
+
+module.exports = fibonacci;
