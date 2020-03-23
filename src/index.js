a0c6e9eb19a3e84f0dab1263efc24b5937701410
L. Felipe Carrillo A <felipe.carrillo1@hotmail.com>
2020-03-22 20:20:52 -0600

    Problema Resuelto

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 const fibonacci = (n) => {
-
+    const array = [n];
+    for(let i=0; i<n; ++i){
+      if(i==0 || i==1) array[i] = 1;
+      else{
+        array[i] = array[i-1] + array[i-2];
+      }
+    }
+    return array;
   }
 
 
