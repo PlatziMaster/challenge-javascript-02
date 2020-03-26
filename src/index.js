0aa3e6864024ab9d04f1989cb29ab41912ae4971
Jared Ortega <jaredo@worcket.com>
2020-03-26 00:07:46 -0600

    Reto solucionado Numero de Fibonacci

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const fibonacci = (n) => {
-
+  const fArray = [n];
+  for(let i = 0; i < n; ++i){
+    (i==0 || i==1) ? fArray[i] = 1 :
+    fArray[i] = fArray[i-1] + fArray[i-2];
   }
+  return fArray;
+}
 
 
-  module.exports = fibonacci;
+module.exports = fibonacci;
