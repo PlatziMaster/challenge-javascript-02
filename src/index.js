1a757e128c5f9456fc1336342e142eab2978147c
Alejandro Lopez <alejozepol@gmail.com>
2020-03-27 10:41:58 -0500

    elaboracion de reto

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 const fibonacci = (n) => {
+  const data = [];
 
+  for (let index = 0; index < n; index++) {
+    if (index === 0) {
+      data.push(0)
+    }
+    else if(index === 1) {
+      data.push(1)
+    } else {
+      data.push(data[index - 2] + data[index - 1])
+    }
   }
+  return data
+}
+
+fibonacci(5)
 
 
-  module.exports = fibonacci;
+module.exports = fibonacci;
