66e476128258aaf0b8181d4f9c99e86ad70c6fd4
Ruben Padilla <rubbenpad2@gmail.com>
2020-03-30 15:40:10 -0500

    solved with memoization

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
-const fibonacci = n => {
+const memoization = (n, memo) => {
+  // Return the n Fibonacci number
   if (n === 0 || n === 1) {
-    return [1]
+    return (memo[n] = 1);
   }
 
-  const sequence = [1, 1]
-
-  for (let i = 2; i < n; i++) {
-    sequence[i] = sequence[i - 1] + sequence[i - 2]
+  if (memo[n]) {
+    return (memo[n] = memo[n]);
   }
 
-  return sequence
-}
+  return (memo[n] = memoization(n - 1, memo) + memoization(n - 2, memo));
+};
 
-module.exports = fibonacci
+const fibonacci = n => {
+  const memo = {};
+
+  memoization(n, memo);
+
+  const result = Object.values(memo);
+
+  // Return a slice from the result
+  // because it start counting from 0
+  return result.slice(0, n);
+};
+
+module.exports = fibonacci;
