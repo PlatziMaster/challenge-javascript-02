9b1cec5ba985d0b9c2c3d6618102e8d154b17893
Jonattan Infante Vargas <infanpearl@gmail.com>
2020-03-29 22:03:29 -0500

    Propuesta de solución serie Fibonacci

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
 const fibonacci = (n) => {
+    // Casos Iniciales
+      let A = 0
+      let B = 1
+    // Array de la Serie
+      let Serie = []
 
-  }
+      //Primeros casos
+      if (n == 0) {
+        Serie.push(A)
+      }else if (n >= 1) {
+        Serie.push(B)
+      }
+
+      
+    //Iteración 
+      let Z = 1
+      while (Z <= n-1) {
+        let C = A + B 
+        Serie.push(C)
+        A = B
+        B = C
+        Z++
+      }
+
+  return Serie
+}
 
 
   module.exports = fibonacci;
