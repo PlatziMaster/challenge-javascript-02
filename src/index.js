d5c0eb50de3dd0600e26c3c52f7069325cf829ff
Alejandro Lopez <alejozepol@gmail.com>
2020-03-30 22:04:46 -0500

    fibonacii con recursividad

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-const fibonacci = (n) => {
-  const data = [];
+const data = [];
 
-  for (let index = 0; index < n; index++) {
-    if (index === 0) {
-      data.push(0)
-    }
-    else if(index === 1) {
-      data.push(1)
-    } else {
-      data.push(data[index - 2] + data[index - 1])
+const fibonacci = (n) => {
+
+  if(data.length > n ){
+    return data.slice(0, n);
+  } 
+  if(n === 1) {
+    data.push(0);
+  } 
+  if(n === 2) {
+    data.push(1);
+  }
+  if(data.length < n){
+    for (let index = data.length; index < n; index++) {
+      data.push(data[index - 2] + data[index - 1]);
     }
   }
-  return data
-}
+    return data;
+  }
 
 module.exports = fibonacci;
