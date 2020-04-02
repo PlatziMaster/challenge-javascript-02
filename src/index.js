244a5c40c1ad398b509222ccfc3c3ecc33f4535a
Alex <ajf@fastmail.com>
2020-04-02 09:52:59 -0600

    Función que devuelve una secuencia de Fibonacci como una array

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
+let sequence = [];
+
 const fibonacci = (n) => {
 
+  /* if (n == 1) {
+    sequence.push(n);
+    return sequence;
+  } else if (n == 2) {
+    sequence.push(1);
+    return sequence;
+  } else if (n > 2) {
+    let sum = sequence[sequence.length-1] + sequence[sequence.length-2];
+    sequence.push(sum);
+  } */
+
+  let nextNumber = sequence[sequence.length-1] + sequence[sequence.length-2];
+
+  if (isNaN(nextNumber)) {
+    sequence.push(1);
+  } else {
+    sequence.push(nextNumber);
   }
 
+  return sequence;
+}
 
-  module.exports = fibonacci;
+
+module.exports = fibonacci;
