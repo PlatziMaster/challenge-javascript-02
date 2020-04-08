10c13931a5bba6d27cf2ce3b7a8487d139e0710c
Andres Campuzano Garzon <30306605+AndresCampuzano@users.noreply.github.com>
2020-04-08 11:55:36 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 const fibonacci = (n) => {
+	const array = [];
+	array.push(n);
+	array.push(n + 1);
+	for (let i = 0; array.length <= 12; i++) {
+		const number = array[array.length - 2] + array[array.length - 1];
+		array.push(number);
+	}
+	return array;
+};
 
-  }
+fibonacci(0); // 0,  1,  1,  2,  3,  5, 8, 13, 21, 34, 55, 89, 144
 
-
-  module.exports = fibonacci;
+module.exports = fibonacci;
