4bc948b64465a6001749ece46be30e40c60e7dbb
hectormr <45437401+HectorMartinezResendiz@users.noreply.github.com>
2020-04-12 12:01:47 -0500

    Resolviendo el challenge de javascript 02

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 const fibonacci = (n) => {
-
+  // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ...
+  let start = 0
+  let end = 1
+  let fibonacciResult
+  let fibonacciArray = []
+  for (i = 0; i < n; i++) {
+    start === 0 ? fibonacciArray.push(end) : fibonacciArray.push(fibonacciResult)
+    fibonacciResult = start + end
+    start = end
+    end = fibonacciResult
   }
+  return fibonacciArray
+}
 
-
-  module.exports = fibonacci;
+module.exports = fibonacci
