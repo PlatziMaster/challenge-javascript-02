fb9e86039a357d79ca078b812eb7373598394a7d
IntiDev <yeseniase@outlook.it>
2020-04-13 23:24:14 -0500

    [ADD] calculo de fibonacci

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 const fibonacci = (n) => {
-
+  let result = [];
+  if (n == 1) {
+    result = [1];
+  } else {
+    result = [1, 1];
+    for (let i = 3; i <= n; i++) {
+      let aux = result[i - 3] + result[i - 2];
+      result.push(aux);
+    }
   }
+  return result;
+};
 
-
-  module.exports = fibonacci;
+module.exports = fibonacci;
