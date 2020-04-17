1c05054dacc189380aa3efc1981349096192ebf3
Esteban Ladino <esteban.ladino.927@gmail.com>
2020-04-16 19:44:58 -0500

    Challenge completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 const fibonacci = (n) => {
-
+  if (n < 0) {
+    return console.error(new Error('No puede ser negativo'));
   }
 
+  let array = [];
 
-  module.exports = fibonacci;
+  for (let i = 0; i <= n; i++) {
+    array.push(fibo(i));
+  }
+
+  return array
+}
+
+const fibo = (n) => {
+  if (n === 0) {
+    return 0;
+  }
+
+  if (n === 1) {
+    return 1;
+  }
+
+  return fibo(n-1) + fibo(n-2);
+}
+
+module.exports = fibonacci;
