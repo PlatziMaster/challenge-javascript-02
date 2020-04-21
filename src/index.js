4f8bbb443a398578932fd066a3e5dba076f0e425
camilo <velandiacamilo356@gmail.com>
2020-04-20 19:59:12 -0500

    resolve

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 const fibonacci = (n) => {
-
+  const result = [];
+  let next;
+  for (let i = 0; i < n; i++) {
+    if (i < 2) {
+      result.push(1);
+    } else {
+      next = result[i - 2] + result[i - 1];
+      result.push(next);
+    }
   }
+  return result;
+};
 
-
-  module.exports = fibonacci;
+module.exports = fibonacci;
