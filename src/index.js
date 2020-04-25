33cb87f48dc835f07662bff21ffac89d114179c4
Edwin García <spark.com.co@gmail.com>
2020-04-25 06:24:42 -0500

    fibonacci function completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 const fibonacci = (n) => {
-
+  const sequence = [];
+  for(let i=0; i<n; i++) {
+    if(i === 0 || i === 1) sequence.push(1);
+    else {
+      const currentNumber = sequence[i-1] + sequence[i-2];
+      sequence.push(currentNumber);
+    }
   }
+  return sequence;
+}
 
-
-  module.exports = fibonacci;
+module.exports = fibonacci;
