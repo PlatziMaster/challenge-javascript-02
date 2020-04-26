20154989211446dc45b5773e90dd8dd7fbf8d2be
sue <sue@MacBook-Pro-de-sue.local>
2020-04-26 16:48:40 -0500

    Giving solution practicing closure topic

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-const fibonacci = (n) => {
-
+const fibonacciClosure = () => {
+  const creatingSerie = (n) => {
+    const serie = [0, 1];
+    for (let i = 2; i <= n; i++) {
+      serie.push(serie[i - 1] + serie[i - 2]);
+    }
+    serie.shift(0);
+    return serie;
   }
+  return creatingSerie;
+
+};
+let fibonacci = fibonacciClosure();
+fibonacci(1);
+
+module.exports = fibonacci;
 
 
-  module.exports = fibonacci;
