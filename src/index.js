6c32651420bfbad55c90eb29ad425b6264e76fac
Bryson Florez <brysonsaman@hotmail.com>
2020-04-27 11:28:08 -0500

    Se realiza la operación fibonacci

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 const fibonacci = (n) => {
+  let a = 0;
+  let b = 1;
+  let c = 0;
+  let result = [];
 
+  for (let i = 0; i < n; i++) {
+    a = b;
+    b = c;
+    c = a + b;
+    result.push(c);
+    debugger
   }
 
+  return result;
+}
 
-  module.exports = fibonacci;
+module.exports = fibonacci;
