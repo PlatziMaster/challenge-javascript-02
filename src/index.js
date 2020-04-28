6f2035f799a1ccd900370ef05ab7301091c7f623
Jeff Barragan <leshz@me.com>
2020-04-27 20:51:07 -0500

    reto 2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
+const getFibonacciNumber = (number) => {
+  if (number <= 1) return 1;
+  return getFibonacciNumber(number - 2) + getFibonacciNumber(number - 1);
+}
+
 const fibonacci = (n) => {
-
+  let result = [];
+  for (let i = 0; i < n; i++) {
+    result.push(getFibonacciNumber(i));
   }
+  return result;
+};
 
-
-  module.exports = fibonacci;
+module.exports = fibonacci;
