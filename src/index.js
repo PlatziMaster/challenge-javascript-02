ca2327ef1691635cbb6d90650aca1ed1f62bb34c
Erickowski <daniel_perez_03081995@hotmail.com>
2020-05-02 17:39:14 -0500

    Challenge solved

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-const fibonacci = (n) => {
-
+const fibonacci = n => {
+  let acum = new Array();
+  for (let index = 0; index < n; index++) {
+    acum.push(calculateNumber(index));
   }
+  return acum;
+}
 
+const calculateNumber = numero => {
+  if (numero <= 1) return 1;
+  return calculateNumber(numero - 1) + calculateNumber(numero - 2);
+}
 
-  module.exports = fibonacci;
+module.exports = fibonacci;
