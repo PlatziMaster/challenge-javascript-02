a92d055866cae473e496f6e11d151e6e273822dd
EmmaIsWorking <dimple.pleod@gmail.com>
2020-05-08 09:34:51 -0500

    reto cumplido

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-const fibonacci = (n) => {
+const fibonacci = (fn) => {
+  let fb = [1, 1];
 
+  if (fn === 1) {
+    return [1];
+  }
+  if (fn === 2) {
+    return fb;
   }
 
+  for (let i = 2; i < fn; i++) {
+    fb.push(fb[fb.length - 2] + fb[fb.length - 1]);
+  }
 
-  module.exports = fibonacci;
+  return fb;
+}
+
+module.exports = fibonacci;
