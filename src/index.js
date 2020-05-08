2c4004fc7fd47833ba2c3f3342a5841f60335963
EmmaIsWorking <dimple.pleod@gmail.com>
2020-05-08 09:37:40 -0500

    custom

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 const fibonacci = (fn) => {
   let fb = [1, 1];
 
-  if (fn === 1) {
+  if (fn === 1)
     return [1];
-  }
-  if (fn === 2) {
+
+  if (fn === 2)
     return fb;
-  }
 
   for (let i = 2; i < fn; i++) {
     fb.push(fb[fb.length - 2] + fb[fb.length - 1]);
