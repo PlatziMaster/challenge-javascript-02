572ba711301eb0ebd60add57802d0f4f058c6c37
Juan Jose Pelaez <juanhernandez.code@gmail.com>
2020-05-08 23:03:28 -0500

    Se hace la seceuncia y se tiene éxito en el test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 const fibonacci = (n) => {
+  var baseSequence = [1, 1]
 
+  for (let i = 0; i < n; i++) {
+    fiboNum = baseSequence[i] + baseSequence[i + 1]
+    baseSequence.push(fiboNum)
   }
 
+  var fibonacciFinal = baseSequence.slice(0, n)
 
-  module.exports = fibonacci;
+  return fibonacciFinal
+
+
+}
+
+
+module.exports = fibonacci;
