2ea18406e7fb07efaf39e888e233b42ae7b7c098
Stiven Jimenez <sajqprogramming@gmail.com>
2020-05-10 11:05:23 -0500

    challenge completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
+// 1, 1, 2, 3, 5, 8, 13 ...
+//cunado n >=2)
+//arrayFido = arrayFido(n-1) + arrayFido(n+2) 
 const fibonacci = (n) => {
-
+  let arrayFibonacci = [0,1];
+  for(let i = 2;i <= n;i++){
+    arrayFibonacci[i]= arrayFibonacci[i - 1] + arrayFibonacci[i - 2]
+  }
+  arrayFibonacci.shift();
+  return arrayFibonacci; 
   }
 
 
