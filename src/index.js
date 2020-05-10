c801d6c14aed54ee82d5b13e2ebc958ee9c84505
Danelia Sanchez <danelia_sanchez20@hotmail.com>
2020-05-10 14:06:04 -0500

    challenge completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-const fibonacci = (n) => {
+const fibonacci = (number) => {
+  let fib1 = 0;
+  let fib2 = 1;
+  let sequenceFib = [];
+  let temp;
 
+  for (let i = 0; i < number; i++) {
+    sequenceFib.push(fib2);
+    temp = fib2;
+    fib2 = fib1 + fib2;
+    fib1 = temp;
   }
 
+  return sequenceFib;
+};
 
-  module.exports = fibonacci;
+module.exports = fibonacci;
