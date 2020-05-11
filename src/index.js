73bb9a26c2a4cc8cd94428b8e6696e430b183811
lenninIbarraGonzalez <soporte@allnexus.com.co>
2020-05-11 12:11:59 -0500

    resolviendo reto fibonacci

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 const fibonacci = (n) => {
-
+  let result = [];
+  if (n === 1) {
+    result = [1];
+  } else {
+    result = [1, 1];
+    for (let i = 3; i <= n; i++) {
+      let aux = result[i - 3] + result[i - 2];
+      result.push(aux);
+    }
   }
-
-
-  module.exports = fibonacci;
+  return result;
+};
+module.exports = fibonacci;
