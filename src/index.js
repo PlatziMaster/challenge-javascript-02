2f5d1cbdbb01ca8c8a46d5b8d7b2594b33786052
hadiazb <hadiazb@unal.edu.co>
2020-05-14 22:04:02 -0500

    Reto solucionado

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 const fibonacci = (n) => {
-
+  let  a = 1; 
+  let  y;
+  let  f = [];
+  for (let i = 0; i < n; i++) {
+    if(i < 2) {
+        f.push(a);
+      }else{
+        y = f[i - 2] + f[i - 1];
+        f.push(y);
+    }
   }
+  return f;
+}
 
 
   module.exports = fibonacci;
