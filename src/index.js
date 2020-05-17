bec781632a5ca6bc6e55724fe8e2512d8fee0a3c
Jorge Ossa <jorgehossa@gmail.com>
2020-05-16 23:41:39 -0500

    Challenge Solved

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 const fibonacci = (n) => {
-
+  let a = [];
+  if(n === 1){
+    a.push(1)
+  }
+  for(let i = 1; i < n; i++){
+      if(i <= 1){
+        a.push(1);
+      }
+      const a1 = a[i - 1]
+      const a2 = a[i - 2] === undefined ? 0 : a[i - 2];
+      a.push(a1 + a2);
   }
 
+  return a
+}
 
-  module.exports = fibonacci;
+module.exports = fibonacci;
