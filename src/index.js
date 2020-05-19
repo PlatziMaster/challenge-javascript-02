af98405bac7b8aa51533090411db2cca853b31e6
Miguel Angel Martelo Quiroz <miguemartelo1031@gmail.com>
2020-05-18 20:23:18 -0500

    Challenge solved

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 const fibonacci = (n) => {
+  const serieFibo = [];
+  let valorSerie0 = 0;
+  let valorSerie1 = 1;
+  let valorIncremento;
 
+  if (n < 2) {
+    serieFibo.push(n);
+  } else {
+    for (let i = 0; i < n; i ++) {
+      valorIncremento = valorSerie0 + valorSerie1;
+      valorSerie0 = valorSerie1;
+      valorSerie1 = valorIncremento;
+      serieFibo.push(valorSerie0);
+    }
   }
 
+  return serieFibo;
+}
 
-  module.exports = fibonacci;
+
+module.exports = fibonacci;
