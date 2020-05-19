4c7b95bb357e768ae157071b699b201de7540828
Alejandro Medina <5almero5@gmail.com>
2020-05-19 18:52:05 -0500

    Add Fibonacci logical
    Using base generator and recursion
    Can be optimized with Memoized Function
    
    and related with this article recursion and generators can be a bad practice
    https://medium.com/javascript-scene/7-surprising-things-i-learned-writing-a-fibonacci-generator-4886a5c87710

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-const fibonacci = (n) => {
+// Generators are a special class of functions that simplify the task of writing iterators.
+function* fib(n, current = 1, next = 1) {
+ // if (n === 0) {
+   // return 0;
+  // }
+  if (n === 0) {
+    return 1;
+   }
+ // segun la este hilo https://github.com/PlatziMaster/challenge-javascript-02/issues/22 no se considerara el 0 
+  yield current;
+  yield* fib(n - 1, next, current + next);
+}
 
-  }
+const fibonacci = (n) => [...fib(n)]
 
-
-  module.exports = fibonacci;
+module.exports = fibonacci;
