a74d55c6697430ce08f9220b3fcdd35a4cd4f5db
Osvaldo Arzate <osvarz.new@gmail.com>
2020-05-21 10:23:22 -0500

    Prueba 01

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
 const fibonacci = (n) => {
-  var secuencia = new Array(n).fill(1)
-  var secuenciaFibonacci = secuencia.map(function () {
-    var a, b, resultado;
-    a = 0;
-    b = 1;
-    resultado = b;
-    for (var i = 1; i <= secuencia.length; i++) {
-      var c = resultado;
-      resultado = a + b;
-      a = b;
-      b = resultado;
-      return c;
-    }
-  })
-  return secuenciaFibonacci
+  var secuencia = [];
+  var a, b, resultado;
+  a = 0;
+  b = 1;
+  resultado = b;
+  for (var i = 1; i <= n; i++) {
+    secuencia.push(resultado);
+    resultado = a + b;
+    a = b;
+    b = resultado;
+  }
+  return secuencia
 }
 fibonacci(10)
 module.exports = fibonacci;
