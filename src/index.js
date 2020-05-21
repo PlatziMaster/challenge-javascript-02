a47a9c8da6eb3543f2d592b028f62b83e4f0db8f
Osvaldo Arzate <osvarz.new@gmail.com>
2020-05-21 09:39:44 -0500

    array de unos :C

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 const fibonacci = (n) => {
-
-  }
-
-
-  module.exports = fibonacci;
+  var secuencia = new Array(n).fill(1)
+  var secuenciaFibonacci = secuencia.map(function () {
+    var a, b, resultado;
+    a = 0;
+    b = 1;
+    resultado = b;
+    for (var i = 1; i <= secuencia.length; i++) {
+      var c = resultado;
+      resultado = a + b;
+      a = b;
+      b = resultado;
+      return c;
+    }
+  })
+  return secuenciaFibonacci
+}
+fibonacci(10)
+module.exports = fibonacci;
