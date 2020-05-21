f29387daa73422a29f26a640f5c404d5ffea2c97
Osvaldo Arzate <osvarz.new@gmail.com>
2020-05-21 10:32:33 -0500

    prueba 03

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const fibonacci = (n) => {
-  const secuencia = [];
-  let a, b, resultado;
+  var secuencia = [];
+  var a, b, resultado;
   a = 0;
   b = 1;
   resultado = b;
-  for (var i = 1; i <= n; i++) {
+  for (let i = 1; i <= n; i++) {
     secuencia.push(resultado);
     resultado = a + b;
     a = b;
