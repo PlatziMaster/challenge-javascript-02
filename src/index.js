7cbdd5d7db7942c24261e1e0b433b8ff48789b13
Alberth Perez <alberth.perez@hotmail.com>
2020-05-23 01:57:58 -0500

    Reto Cumplido

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 const fibonacci = (n) => {
+    
+  var array = [n];
 
-  }
-
-
-  module.exports = fibonacci;
+  for (var i = 0; i < n; i++) {
+    if (i >= 0 && i <= 1){
+        if(i === 0){
+          array[i] = 1;
+        }else{
+          array[i] = 1;
+        }
+      }else{
+          array[i] = array[i-1] + array[i-2];        
+      }
+    }
+  return array;       
+}
+  
+module.exports = fibonacci;
