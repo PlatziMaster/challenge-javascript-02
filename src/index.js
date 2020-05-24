0854fe7ff8c185a44b2a14d8c35635410b99af56
Ernesto Lazaro <ernestolazaro84@gmail.com>
2020-05-23 22:02:16 -0500

    Challenge 2 of JS Passed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 const fibonacci = (n) => {
 
+  const array = [n];
+  for (let i = 0; i < n; i++) {
+ 
+    if( i < 2) {
+      array[i] = 1;
+    } else {
+   array[i] = array[i - 1] + array[i-2];
+  }
+}
+    return array;
   }
 
-
-  module.exports = fibonacci;
+module.exports = fibonacci;
