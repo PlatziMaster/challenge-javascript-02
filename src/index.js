909ab36731cd209b2b84822b859bae1c8f6ac67d
Oscar Palomino <ing.oscarp1@gmail.com>
2020-05-25 15:49:44 -0500

    Challenge JS 2 solved

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 const fibonacci = (n) => {
-
+  const sequence = []
+  let a = 1
+  let b = 0
+  let tmp
+  sequence.push(b)
+  sequence.push(a)
+  while(sequence.length < n){
+    tmp = a
+    a += b
+    b = tmp
+    sequence.push(a)
   }
+  return sequence
+}
 
 
   module.exports = fibonacci;
