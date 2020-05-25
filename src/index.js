1042c478c5a0ef36e336be97326027a9190d666b
Abdiel Ortega <nathanabdiel1@gmail.com>
2020-05-24 19:04:35 -0500

    The challenge was completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const fibonacci = (n) => {
-
+  const fibonacciSerie = [0, 1];
+  for (let index = 2; index <= n; index++) {
+    fibonacciSerie.push(fibonacciSerie[index - 1] + fibonacciSerie[index - 2])
   }
+  fibonacciSerie.shift();
+  return fibonacciSerie;
+}
 
 
-  module.exports = fibonacci;
+module.exports = fibonacci;
