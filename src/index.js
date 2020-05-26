87a83797a6f28cc1e81f183e4e022a3c7ac1551c
alejandromunozc <alejandromunozc@gmail.com>
2020-05-26 18:09:31 -0500

    Función que entrega en un Array la secuencia Fibonacci de un numero n

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 const fibonacci = (n) => {
+    fib = new Array(n).fill(0);
+    if (n < 2) {
+        fib[0] = n;
+        return fib;
+    } else {
+        fib[0] = 1;
+        fib[1] = 1;
+        for (let i = 2; i < n; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+    }
+    return fib;
+}
 
-  }
-
-
-  module.exports = fibonacci;
+module.exports = fibonacci;
