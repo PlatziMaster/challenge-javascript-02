02139337a5a47e5caf87ef4ad1ed46a8190c327c
afcarrion <af.carrion@uniandes.edu.co>
2020-05-26 08:36:10 -0500

    Secuencia Fibo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 const fibonacci = (n) => {
+  let fibo = [1, 1];
 
+  if (n === 1) {
+    return (fibo = [1]);
+  } else if (n === 0) {
+    return (fibo = [0]);
+  } else if (n > 1) {
+    for (let i = 2; i < n; i++) {
+      fibo[i] = fibo[i - 2] + fibo[i - 1];
+    }
+    return fibo;
   }
-
-
-  module.exports = fibonacci;
+};
+module.exports = fibonacci;
