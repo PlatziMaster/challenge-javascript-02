bf52e9d1c685417c49970dfefdf2b23c3cf93751
Carlos Mora <contacto@carlosmora.biz>
2020-05-27 23:50:07 -0500

    [SOLVED]: fibonacci working

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 const fibonacci = (n) => {
-
+  let a = 0;
+  let b = 1;
+  for (let index = 0; index < n; index++) {
+    let c = b + a ;
+    return c;
+    a = b;
+    b = c;
   }
-
+  }
+  // fibonacci(20)
 
   module.exports = fibonacci;
