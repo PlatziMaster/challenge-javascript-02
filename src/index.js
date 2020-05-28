bdef7cfef880bf04fc1c3ab769dff01ed07f9dbb
Jhon Carlos Colorado <8047140+jcolorado19@users.noreply.github.com>
2020-05-28 14:34:08 -0500

    Solved challenge Number Finobacci

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 const fibonacci = (n) => {
+  let result = [1];
+  let array = [1, 1];
 
+  if (n == 2) {
+    result = array;
+  }else if(n > 2){
+      for(let j = 2; j < n; j++){
+        let prev = array[j - 2];
+        let next = array[j - 1];
+        suma = prev + next
+        array.push(suma)
+      }
+      result = array;
   }
+  return result;
+}
+
+
 
 
   module.exports = fibonacci;
