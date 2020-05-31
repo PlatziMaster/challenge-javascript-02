a63c6f2e8739aa83fa86c66a7f481c1e819f4890
Carlos Perez <carlos.perez@charliegperez.com>
2020-05-31 17:59:32 -0500

    challenge solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 const fibonacci = (n) => {
+  let valorI = 1;
+  let valorII = 1;
+  let valorActual = 0;
+  let fibonacciArray = [];
+  for (let index = 1; index <= n; index++) {
+    valorActual = valorII;
+    valorII = valorI;
+    valorI = valorI + valorActual;
+    fibonacciArray.push(valorActual);
+  };
+  return fibonacciArray;
+};
 
-  }
-
-
-  module.exports = fibonacci;
+module.exports = fibonacci;
