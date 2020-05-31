32e0e2cff4a1adec695ae189405ce501e7b6e3c6
Sergio Leal <checholeal7@gmail.com>
2020-05-31 14:38:52 -0500

    Fibonacci sequence

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 const fibonacci = (n) => {
 
+  let arr = [0, 1];
+  for (let i = 2; i < n + 1; i++){
+    arr.push(arr[i - 2] + arr[i -1])
+  }
+ return arr
+ 
+
   }
 
+ 
 
-  module.exports = fibonacci;
+
+
+module.exports = fibonacci;
