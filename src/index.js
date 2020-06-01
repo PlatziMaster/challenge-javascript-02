8565c0a283159203a29ff0804ae0ca21938397b4
Carlos Mora <github@carlosmora.biz>
2020-06-01 07:36:09 -0500

    :white_check_mark:[REFACTOR]: Challenge Solved
    change the code to add the array solution

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 const fibonacci = (n) => {
   let a = 0;
   let b = 1;
-  let x = 1;
-  for (let index = 0; index < n; index++) {
-    let c = b + a ;
-    // console.log(c);
-    x = x + c;
+  let c = b;
+  let x = [];
+  x[0] = c;
+  for (let index = 1; index < n; index++) {
+    c = b + a;
     a = b;
     b = c;
+    x[index] = c;
+    }
+    return x;
   }
-  }
-  // fibonacci(20)
 
   module.exports = fibonacci;
