641228f1c5e53f54107e8a837dfb9ab9b0c31b4d
Luis LC <luis.lazcanocruz@gmail.com>
2020-06-03 23:31:20 -0500

    Reto Cumplido

--- src/index.js
+++ src/index.js
@@ -1,6 +1,38 @@
 const fibonacci = (n) => {
 
+  return repeat(n, fibonacciFormule);
+
+  function repeat(times, action){
+    let result=[];
+    for(let i=1; i<=times; i++){
+      number =action(i);
+      result.push(number);
+    }
+    return result;
   }
 
+  function fibonacciFormule(f){
+    if(!this.cache){
+      this.cache = {}
+    }
 
-  module.exports = fibonacci;
+    if(this.cache[f]){
+      return this.cache[f]
+    }
+
+    if(f <= 0){
+      return 0;
+    }
+    else if(f===1){
+      return 1;
+    }
+    else{
+      this.cache[f] = (fibonacciFormule(f-1) + fibonacciFormule(f-2));
+      return this.cache[f];
+    }
+  }
+}
+
+
+
+module.exports = fibonacci;
