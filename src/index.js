5099d7ea9fea08ffa64d291422175c0297b9c03d
Santiago Forero <ds.foreroa@uniandes.edu.co>
2020-06-04 14:18:54 -0500

    challenge completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
-const fibonacci = (n) => {
+const fibonacci = (n) =>{ 
+ 
+    let arr=[1];
+    let num1=1;
+    let num=0;
+    for (let index = 0;n>1 && index <= n-2; index++) {
+      newNum=num+num1;
+      arr.push(newNum);
+      num=num1;
+      num1=newNum
 
-  }
+    }
+    console.log(arr);
+    return arr;
+   
+  
+};
 
 
   module.exports = fibonacci;
