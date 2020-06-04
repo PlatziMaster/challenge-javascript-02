15bd03aa90bbadc820bedfe0038efa6ed1173fd1
luiscolungaperez <perez.luis.colunga@gmail.com>
2020-06-03 21:33:58 -0500

    Done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 const fibonacci = (n) => {
-
+  const secuencia = [0, 1];
+  for (let i = 0; i < n - 1; i++) {
+    let newNumber = 0;
+    newNumber = secuencia[i] + secuencia[i + 1];
+    secuencia.push(newNumber);
   }
+  secuencia.shift();
+  return secuencia;
+};
 
-
-  module.exports = fibonacci;
+module.exports = fibonacci;
