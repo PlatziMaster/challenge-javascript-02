eb00e5fa6e718ec9120af6a455d170cf542b2192
Diego Valdez <dgogo9@hotmail.com>
2020-06-05 20:25:44 -0500

    Challenge completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const fibonacci = (n) => {
-
+    let arreglo = [];
+    for(let i = 0; i < n; i++){
+      (i === 0) ? arreglo.push(1) : arreglo.push(arreglo[i-2] + arreglo[i-1] || 1)
+    }
+    return arreglo;
   }
-
 
   module.exports = fibonacci;
