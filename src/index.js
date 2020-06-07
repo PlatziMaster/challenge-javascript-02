070f9a4682034b196cba28ab937f70e10fc3db5b
Diego Rodriguez <alejandro27062@gmail.com>
2020-06-07 14:16:33 -0500

    challenge completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const fibonacci = (n) => {
-
+  numbers = [0, 1]
+  for(i = 1; i < n; i++) {
+    numbers.push(numbers[i - 1] + numbers[i])
   }
-
-
+  numbers.shift()
+  return numbers
+}
   module.exports = fibonacci;
