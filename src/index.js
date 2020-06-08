e29d6b9e7dae788c5186e760ab5e6940c737d5b7
Andres Moreno <andresmorenojf@gmail.com>
2020-06-08 10:24:34 -0500

    Reto 02 - Resuelto

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 const fibonacci = (n) => {
-
+  const finonacciChallenge = [];
+  let a = 1;
+  let b = 1;
+  for (let i = 1; i <= n; i++) {
+    finonacciChallenge.push(a);
+    let c = a + b
+    a = b;
+    b = c;
   }
 
+  return finonacciChallenge;
+}
 
-  module.exports = fibonacci;
+
+module.exports = fibonacci;
