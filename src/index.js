3171ab9a6e610341ea43a1ab525bb4ca33cb3ca9
frakxman <frakxman@hotmail.com>
2020-06-09 00:37:43 -0500

    Challenge-javascript-02 Ok

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-const fibonacci = (n) => {
+const fibonacci = ( n ) => {
+  let a = 0, b = 1, c = 1, fibonacciArray = [];
+	for ( let i = 0; i < n; i++ ) {
+		a = b;
+		b = c;
+		c += a;
+		fibonacciArray.push ( a );
+	};
+    return fibonacciArray;
+}
 
-  }
-
-
-  module.exports = fibonacci;
+module.exports = fibonacci;
