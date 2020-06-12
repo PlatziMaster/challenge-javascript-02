a409077c7fe176158d5104b6292de17f28b51b63
den <den.velez@gmail.com>
2020-06-12 10:41:02 -0500

    it is solved

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 const fibonacci = (n) => {
-
+  let fibonacciArray = [];
+  for (let i = 0; i < n; i++) {
+    if (i <= 1) {
+      fibonacciArray.push(1);
+    } else {
+      const a = i - 1;
+      const b = i - 2;
+      const suma = fibonacciArray[a] + fibonacciArray[b];
+      fibonacciArray.push(suma);
+    }
   }
+  return fibonacciArray;
+};
 
-
-  module.exports = fibonacci;
+module.exports = fibonacci;
