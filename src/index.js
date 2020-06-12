7a6b90f8f9f2a1088690e425744e79e42db89ba0
Daniel Arrubla <darrubla@uninorte.edu.co>
2020-06-12 10:06:27 -0500

    fibonacci challenge solved

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-const fibonacci = (n) => {
-
+const fibonacci = (num) => {
+  let arrnum = [];
+  for (let i = 0; i < num; i++) {
+    i == 0 ? arrnum.push(1) : arrnum.push(arrnum[i - 2] + arrnum[i - 1] || 1);
   }
+  return arrnum;
+};
 
-
-  module.exports = fibonacci;
+module.exports = fibonacci;
