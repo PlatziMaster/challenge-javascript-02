85cf691e9b5e6589cae3b4f4a6308f5903d7514c
Rafael Alvarez <tecnoportuaria@gmail.com>
2020-06-12 16:44:09 -0500

    reto complido by: soyraffaello

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-const fibonacci = (n) => {
 
+
+const fibonacci = (n) => {
+    let numeros=[0,1];
+    for (let i = 2; i < n; i++) {
+        numeros[i] = numeros[i - 2] + numeros[i - 1];
+    }
+    return numeros;
   }
 
 
