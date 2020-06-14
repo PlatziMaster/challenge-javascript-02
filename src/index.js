c96a5eb61d08567d9dc0874fb512880573ca238e
Angel Castiblanco <angelcastiblanco96@gmail.com>
2020-06-14 14:23:59 -0500

    Reto completado

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
+let fiboArray = [];
+let lastNumberOne;
+let lastNumberTwo = 0;
 const fibonacci = (n) => {
-
+  for (let i = 0; i <= n; i++) {
+    if(fiboArray.length === 0) {
+      fiboArray.push(n);
+      lastNumberOne = n
+    }
+    else{
+      let result = lastNumberOne + lastNumberTwo
+      lastNumberTwo = lastNumberOne
+      lastNumberOne = result
+      fiboArray.push(result)
+    }
+    return fiboArray
+  }
   }
 
 
