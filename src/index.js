ab737306bbb9f5853e5ef7a8170a44ebfe8e6569
John D. Gómez H <jdgomezh@gkodo.com>
2020-06-14 00:54:17 -0500

    feat(fibonacci): Implementation of the fibonacci calculation The algorithm is implemented to calculate the "N" values of the fibonacci sequesnce for a given "N", where n is an integer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 const fibonacci = (n) => {
+  const serie = [0,1];
 
+  for (let i=1; i < n; ++i) {
+    let [ last, now ] = [
+      serie[serie.length-2],
+      serie[serie.length-1]
+    ];
+
+    let next = last + now;
+    serie.push(next)
   }
 
+  serie.shift();
+  return serie;
+}
 
-  module.exports = fibonacci;
+
+module.exports = fibonacci;
