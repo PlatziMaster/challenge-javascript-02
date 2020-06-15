3fbc5ad5770a8737a63c749423ed3728d657f40f
Alexander PC <jhonalexander9@gmail.com>
2020-06-15 17:47:49 -0500

    solved challenge

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 const fibonacci = (n) => {
-
+  let array = []
+  let a = 1
+  let b = 1
+  for (let i = 0; i < n; i++) {
+    array.push(a)
+    let c = a + b;
+    a = b;
+    b = c;
   }
+  console.log(array);
+  return array
+}
+
+fibonacci(10);
 
 
-  module.exports = fibonacci;
+module.exports = fibonacci;
