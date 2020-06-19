96575e7c59ad6880d4c190a5a5074f4603e2b4c8
Gabriel <gabospa@gmail.com>
2020-06-19 00:26:40 -0500

    Problem 2 Solved

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 const fibonacci = (n) => {
-
-  }
+  let array = []  
+    for(let i = 0; i < n; i++){
+        if (i < 2){
+          array[i] = 1
+        }
+        else if (i > 1) {
+          array[i] = array[i-1] + array[i-2]
+        }
+      }
+    return array
+}
+  
 
 
   module.exports = fibonacci;
