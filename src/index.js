b18a01541e37bafe7741da51a48f3a2f2b4cfa1f
Natanael <nj.leon.14@gmail.com>
2020-06-26 02:46:26 -0500

    reto fibonacci completado, confieso que no lo entendí a la primera

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 const fibonacci = (n) => {
-
-  }
-
+  const arrayFibo = [0, 1];
+  for (let i = 0; i < n - 1; i++) {
+    let nextNumber = 0;
+    nextNumber = arrayFibo[i] + arrayFibo[i + 1];
+    arrayFibo.push(nextNumber);
+  }	  
+  arrayFibo.shift();
+  return arrayFibo;
+}
 
   module.exports = fibonacci;
