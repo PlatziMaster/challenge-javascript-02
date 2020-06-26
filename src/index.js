0b3d92f07d2f9a4e8cf8a0cae787e4907edbc448
Moilu <moisesluna0390@gmail.com>
2020-06-25 19:23:47 -0700

    Va el segundo :D

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 const fibonacci = (n) => {
+  
+  let numeros=[0,1];
+
+  for (let i = 2; i < n; i++) 
+  {
+      numeros[i] = numeros[i - 2] + numeros[i - 1];
+  }
+  
+  return numeros;
+
 
   }
-
-
   module.exports = fibonacci;
