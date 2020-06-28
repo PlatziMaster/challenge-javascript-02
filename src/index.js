fd159d2f5e9cf13e00b3b9a955b05da3a101d964
Felipe Chávez <a.philippc@gmail.com>
2020-06-28 13:58:08 -0500

    Challenge Completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 const fibonacci = (n) => {
 
+  const sequence = [0, 1];
+  for(let i = 0; i < n - 1; i++) {
+    let newNumber = 0;
+    newNumber = sequence[i] + sequence [i + 1];
+    sequence.push(newNumber);
   }
-
+  sequence.shift();
+  return sequence;
+  }
 
   module.exports = fibonacci;
