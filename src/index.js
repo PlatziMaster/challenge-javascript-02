edcd1bcafaaf1e36ea362bafa3aab101402987f0
Juan Espitia <juanespitia92@hotmail.com>
2020-07-01 21:03:52 -0500

    Se agrega funcion

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 const fibonacci = (n) => {
+var laentrega= new Array(n+1);
+laentrega[0]=0;
+laentrega[1]=1;
 
+
+for( var i = 2; i <= (n-1) ; i++){
+
+        laentrega[i]=( laentrega[i-1] + laentrega[i-2] );
+
+}
   }
 
 
