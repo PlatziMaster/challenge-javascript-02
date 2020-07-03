e405ba2be26eb0f5e4be051a366d2c920620c05a
Daniel V.M <Daniel@MacBook-Air-GEARS.local>
2020-07-02 23:29:44 -0500

    challenge 02 ready

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const fibonacci = (n) => {
-
+  function* functionFibonacci() {
+    let numeroBase = [1];
+    yield numeroBase;
+    (numeroBase.push(1))
+    yield numeroBase;
+    while (true) {
+      (numeroBase.push((numeroBase[numeroBase.length - 1]) + (numeroBase[numeroBase.length - 2])))
+      yield numeroBase;
+    };
+  };
+  let retornaValor = functionFibonacci();
+  let resultadoFibonacci
+  for (let i = 1; i <= n; i++) {
+    resultadoFibonacci = retornaValor.next().value;
+  };
+  return resultadoFibonacci;
   }
 
 
