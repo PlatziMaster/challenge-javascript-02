dff92038664c1bfea9b69d56724e7d119726c72b
Alejandro Ortiz Duque <davidalejandro64@gmail.com>
2020-07-04 12:43:09 -0500

    create fibo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
-const fibonacci = (n) => {
-
+const fibonacci = (n, memo={}) => {
+  if (n === 0 || n === 1){
+    return 1;
+  } else {
+    try {
+      return memo[n]
+    } catch {
+      resultado = fibonacci(n-1, memo) + fibonacci(n - 2, memo);
+      memo[n] = resultado;
+      return resultado;
+    }
   }
 
 
