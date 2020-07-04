6c51a2c56c44560d0400fb751359a6b7da59ce68
Alejo Ortiz <davidalejandro64@gmail.com>
2020-07-04 13:15:24 -0500

    update index to return an array

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 const fibonacci = (n, memo={}) => {
-  if (n === 0 || n === 1){
-    return 1;
-  } else {
-    try {
-      return memo[n]
-    } catch {
-      resultado = fibonacci(n-1, memo) + fibonacci(n - 2, memo);
-      memo[n] = resultado;
-      return resultado;
+  let fibo = [];
+  for(let i = 0; i < n; i++) {
+    if (i <= 1){
+      fibo.push(1);
+    } else {
+      let resultado = fibo[i - 1] + fibo[i - 2];
+      fibo.push(resultado);
     }
   }
+  return fibo;
+}
 
-
-  module.exports = fibonacci;
+module.exports = fibonacci;
