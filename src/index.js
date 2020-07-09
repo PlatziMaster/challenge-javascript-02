7031a1c150817e6f881358e9271041d9688fa806
chato1337 <chato1337@gmail.com>
2020-07-09 09:50:09 -0500

    solucion al reto por didier cuetia

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 const fibonacci = (n) => {
+  
+  var serieFibonacci = [];
+  for (let i = 0; i < n; i++) {
+      if(i === 0 || i === 1){
+        serieFibonacci[i] = 1;
+      } else if(i>1){
+        serieFibonacci[i] = serieFibonacci[i-1] + serieFibonacci[i-2];
+      }
+    }
 
+    return serieFibonacci;
   }
 
 
