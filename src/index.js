9958326f3c288c597bedbae23da6c68888057516
Gerardo Garavito <garavitogerardo@gmail.com>
2020-07-16 18:25:28 -0500

    challenge02 done - gerardogaravito

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 const fibonacci = (n) => {
+  var arrayFibo = [1];
 
+  if (n == 1){
+    return arrayFibo;
+  }else if(n == 2){
+    arrayFibo.push(1);
+    return arrayFibo;
+  }else {
+    arrayFibo.push(1);
   }
 
+  for (var i=2; i < n; i++ ){
+    arrayFibo.push(arrayFibo[i-2] + arrayFibo[i-1]);
+  }
+  return arrayFibo;
+}
 
-  module.exports = fibonacci;
+
+module.exports = fibonacci;
