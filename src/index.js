d4a5612784f01356fafebd4014cbb0103847b762
Maorguez <avila.mauro@gmail.com>
2020-07-17 18:16:44 -0500

    Challenge solved

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 const fibonacci = (n) => {
-
+  var a = 1, b = 0, serie = [], temp;
+  for (let i = 0; i < n; i++) {
+    temp = a;
+    a += b;
+    b = temp;
+    serie[i] = b;
   }
+  return serie;
+}
+
+console.log(fibonacci(14));
 
 
   module.exports = fibonacci;
