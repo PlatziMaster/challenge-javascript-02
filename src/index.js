dc0a10e98473e5103aaf7a8020a44b01f4a2d233
Paul Montoya <52388061+PaulMoro@users.noreply.github.com>
2020-07-18 22:31:25 -0500

    Hecho

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
+
 const fibonacci = (n) => {
-  let fibonacci = []
-  a = 0
-  b = 1
-  result = b
-  for (i = 1; i < 11; i++)
-    fibonacci.push (result)
-    result = a+b
-    a = b
-    b = result
+  const arrayFb = [1, 1]
+  if(n === 1) {
+    return [1];
+  } if (n === 2) {
+    return [1, 1];
+  } if (n > arrayFb.length) {
+    for (let i = 0; i < n - 2; i++) {
+      arrayFb.push(arrayFb[i] + arrayFb[i + 1]);
+      return arrayFb;
+    }
+    return arrayFb;
   }
-
-
-  module.exports = fibonacci;
+}
+module.exports = fibonacci;
