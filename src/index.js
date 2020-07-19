2f03b2fa4046a923dc7de5f30e4880120bf9d6a5
Paul Montoya <52388061+PaulMoro@users.noreply.github.com>
2020-07-18 21:38:57 -0500

    test one

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 const fibonacci = (n) => {
-
+  let fibonacci = []
+  a = 0
+  b = 1
+  result = b
+  for (i = 1; i < 11; i++)
+    fibonacci.push (result)
+    result = a+b
+    a = b
+    b = result
   }
 
 
