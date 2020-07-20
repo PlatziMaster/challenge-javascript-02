2ad8ad2459ef93709677ad8b93109a2b15cbc4fb
adriangutir7 <adriangutir@gmail.com>
2020-07-19 23:52:50 -0500

    Add solution to cahallenge-javascript-02 Platzi Master

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 const fibonacci = (n) => {
-
+  let fibonacciArray = [];
+  fibonacciArray[0] = 0;
+  fibonacciArray[1] = 1;
+  for (i = 2; i <= n; i++) {
+    fibonacciArray[i] = fibonacciArray[i - 1] + fibonacciArray[i - 2];
   }
+  fibonacciArray.splice(0, 1);
+  return fibonacciArray;
+};
 
-
-  module.exports = fibonacci;
+module.exports = fibonacci;
