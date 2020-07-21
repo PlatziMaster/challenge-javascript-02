1cbd5697ec57d6e1ea29e41e6fc6b9b4e1e9fc4c
akero11 <luisangelw3@outlook.com>
2020-07-20 21:16:51 -0500

    Code improved

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const fibonacci = (n) => {
-  var fib = [0,1];
-  for(i=0; i<n-1; i++){
-    fib.push(fib[i]+fib[i+1]);
+  var fib = [1,1];
+  for(i=2; i<n; i++)
+  {
+    fib.push(fib[i-2]+fib[i-1]);
   }
-  return (fib.slice(-n));
+  return fib.slice(-n);
 }
 
   module.exports = fibonacci;
