655b37a307ae7b2b60a48e9c7841e2f7e810ee00
Pitupan <rojas.fredic6@gmail.com>
2020-07-22 19:08:02 -0500

    Realizacion ejercicio challenge-2 de JavaScript

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 const fibonacci = (n) => {
-
+    let fibonacciArray = [];
+    fibonacciArray.push(n);
+    for(var i=0; i<20; i++){
+        let fibonacciArrayLenght = fibonacciArray.length;
+        let fibonacciLastValue = fibonacciArray[fibonacciArrayLenght - 1];
+        let fibonacciAlmostLastValue = fibonacciArray[fibonacciArrayLenght -2];
+        if(fibonacciArrayLenght<2){
+            fibonacciArray.push(fibonacciLastValue+1);
+        }else{
+            fibonacciArray.push(fibonacciLastValue + fibonacciAlmostLastValue);
+        };
+    }
+    return fibonacciArray;
   }
+
+console.log(fibonacci(0));
 
 
   module.exports = fibonacci;
