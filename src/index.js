f2807fd7363af777c1fc8a0f6d351929a8fd73ce
Jobana Garavito Q <nellyjobana@gmail.com>
2020-07-25 13:48:09 -0500

    Fibonacci Numbers by Jobana Garavito

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 const fibonacci = (n) => {
-
+     
+    var fib = [];
+    fib[0] = 1;
+    fib[1] = 1;
+  
+    if (n <= 1) {
+        fib.pop()
+        return fib
+    }
+  
+    for(var i=2; i<n; i++) { 
+      fib[i] = fib[i-2] + fib[i-1];
+    }
+    return fib
   }
-
-
   module.exports = fibonacci;
