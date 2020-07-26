52782190e3783c0ae76ae2d65cc24c30506d543b
Sergio Ruiz <shecho27@gmail.com>
2020-07-26 16:44:17 -0500

    Edit the function: Fibonacci's sequence

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 const fibonacci = (n) => {
+  var fibonacci = [];
+  fibonacci[0] = 0;
+  fibonacci[1] = 1;
 
+  if( n > 1 ){
+    for (var i = 2; i < n; i++) {
+      fibonacci[i] = fibonacci[i-2] + fibonacci[i-1];
+    }
+    return fibonacci;
   }
+}
 
-
-  module.exports = fibonacci;
+module.exports = fibonacci;
