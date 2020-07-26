543d8962bb665e5a757a53bf7d00895611b8733e
Sergio Ruiz <shecho27@gmail.com>
2020-07-26 18:27:56 -0500

    Edit the function: Fibonacci's sequence Try 2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 const fibonacci = (n) => {
-  var fibonacci = [];
-  fibonacci[0] = 1;
-  fibonacci[1] = 1;
+  var fibonacci = [0 , 1];
 
   for (var i = 2; i < n; i++) {
     fibonacci[i] = fibonacci[i-2] + fibonacci[i-1];
