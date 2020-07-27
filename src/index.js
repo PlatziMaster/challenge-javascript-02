5c0421215f13008fdf890fc6bc783c0d61a83e6a
María Andrea <pericoandrea@gmail.com>
2020-07-26 20:25:40 -0500

    Fibonacci Sequence Algorithm

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 const fibonacci = (n) => {
 
+  const fibonacciArray = [0,1]
+  if(n > 0) {
+    for (let index = 2; index <= n; index++) {
+      fibonacciArray.push(
+        fibonacciArray[index-1] + fibonacciArray[index-2] 
+      );
+    }
+    fibonacciArray.shift()
+    return fibonacciArray;
   }
+  return;
+}
 
 
   module.exports = fibonacci;
