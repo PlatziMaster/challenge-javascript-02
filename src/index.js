524882840bc0866b492d5b62ec197c993448fffc
johan-avila <johan-avila@outlook.com>
2020-07-31 22:36:55 -0500

    reto cumplido johan-avila@outlook.com

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 const fibonacci = (n) => {
-
+  let array = [1,1]
+  
+  for(let i=0;i<n;i++){
+    
+    let count = array[i]+array[i+1]
+    array.push(count)
+    
   }
+  array.length=n
+  return array
+
+}
 
 
-  module.exports = fibonacci;
+module.exports = fibonacci;
