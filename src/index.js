1aef6d2afa3d2a4a66b0035167990f4a7d5bb9e5
carlosvldz <me@carlosvldz.com>
2020-08-14 01:17:45 -0500

    carlosvldzzz - challenge completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 const fibonacci = (n) => {
 
-  }
+  let array = [0,1]
+  let coun
 
+  while (n > 0){
+    coun = array[array.length-1]+array[array.length-2]
+    array.push(coun)
+    n--
+  }
+    array.pop()
+    array.shift()
+
+    return(array)
+}
 
   module.exports = fibonacci;
