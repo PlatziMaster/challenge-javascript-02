186fcfe7fb82fcc36cfefec3c9b16b068e5178e6
Jesus Garcia <jelegaho@gmail.com>
2020-08-24 22:04:09 -0500

    challenge finished

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const fibonacci = (n) => {
-
+  let fib = [0, 1]
+  for (let i = 2; i <= n; i++){
+    fib.push(fib[i-1] + fib[i-2])
   }
+  fib.shift()
+  return fib
+}
 
-
-  module.exports = fibonacci;
+module.exports = fibonacci;
