c1af2b0643e20e0a4ebf710887a85fe182820d1d
Efrain Hernandez <efrainhg2050@gmail.com>
2020-08-27 22:05:20 -0500

    Solución al reto 2 de JavaScript

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
-const fibonacci = (n) => {
-
+  const fibonacci = (n) => {
+    let arr = [];
+    if(n === 0)
+      return 0;   
+    if(n === 1) {
+      arr.push(1)
+      return arr;
+    }
+      
+      
+    if(n === 2) {
+      arr.push(1,1)
+      return arr;
+    }
+      
+    if(n > 2) {
+      arr.push(1,1)
+      let fibonacciNumber = 0
+    
+      for(let index = 1; index <= n-2; index++) {
+        fibonacciNumber = arr[index] + arr[index - 1]
+        arr.push(fibonacciNumber)
+      }
+      return arr;
+    }
   }
-
 
   module.exports = fibonacci;
