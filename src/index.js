011d2acf12da317caee72e32f81de92695d92c4c
Israel Castro <israel.castro@gmail.com>
2020-09-19 02:44:59 -0500

    [Done] Challenge JS 02

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const fibonacci = (n) => {
-
+  let fibArray = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    fibArray.push(fibArray[i - 1] + fibArray[i - 2]);
   }
+  fibArray.shift();
+  return fibArray;
+};
 
-
-  module.exports = fibonacci;
+module.exports = fibonacci;
