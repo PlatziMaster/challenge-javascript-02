087b51434a0fd0b54121ed42d4fbab32170945a0
Amaury Soto <amaurysotog@gmail.com>
2021-04-26 16:01:46 -0500

    Logic Amaury fibonacci

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 const fibonacci = (n) => {
 
+  const arrayFibonacci = [0, 1]
+  if (n > 0) {
+    for (let index = 2; index <= n; index++) {
+      arrayFibonacci.push(
+        arrayFibonacci[index - 1] + arrayFibonacci[index - 2]
+      );
+    }
+    arrayFibonacci.shift()
+    return arrayFibonacci;
+
+
   }
+  return
+}
 
 
-  module.exports = fibonacci;
+
+
+
+module.exports = fibonacci
