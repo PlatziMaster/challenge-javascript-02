92958633804a9292390840e6d503ac525299bbb6
Stiven Jiménez <58489695+stivenjimenez@users.noreply.github.com>
2021-05-18 11:43:19 -0500

    test

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 const fibonacci = (n) => {
-  let arrayFibonacci = [0,1];
-  for(let i = 2;i <= n;i++){
-    arrayFibonacci[i]= arrayFibonacci[i - 1] + arrayFibonacci[i - 2]
+  let arrayFibonacci = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    arrayFibonacci[i] = arrayFibonacci[i - 1] + arrayFibonacci[i - 2];
   }
   arrayFibonacci.shift();
-  return arrayFibonacci; 
-  }
+  return arrayFibonacci;
+};
 
-
-  module.exports = fibonacci;
+module.exports = fibonacci;
