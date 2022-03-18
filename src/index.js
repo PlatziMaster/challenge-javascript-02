daf22aa6deac27b3e7ccc813315ad5310e2dc613
Juan Alberto Solis Castro <juansolistech@gmail.com>
2022-03-18 08:15:50 -0300

    solucion ch-02

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 const fibonacci = (n) => {
+    arrayFibonacci = [0,1];
+    for (i=2; i<=n; i++) {
+      arrayFibonacci[i] = arrayFibonacci[i-1] + arrayFibonacci[i-2]
+    };
+    arrayFibonacci.shift();
+    return arrayFibonacci
+
 
   }
 
